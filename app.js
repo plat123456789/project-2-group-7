4bd733933b0eeaaeebdaf25b2ee6946eb4291704
plat123456789 <plat123456789@qq.com>
2018-07-09 11:17:42 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('expres')
+const bodyParser = require('body-parser')
+const app = express();
+
+const user = [
+    {
+        id: 0,
+        email: "tom@tom.com",
+        password: "tom"
+    },{
+        id: 1,
+        email: "peter@peter.com",
+        password: "peter"
+    }
+];
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use('local-login', new LocalStrategy(
+    async (email, password, done) => {
+        try{
+            let users = await knex('users').where({email:email});
+            if (users.length == 0) {
+                return done(null, false, { message: 'Incorrect credentials.' });
+            }
+            let user = users[0];
+            if (user.password === password) {
+                return done(null, user);
+            }else{
+                return done(null, false, { message: 'Incorrect credentials.' });
+            }
+        }catch(err){
+            return done(err);
+        }
+    }
+));
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.listen(3000)
