c0ed197de32067100734ce1c5533d8a535790218
Michelle Lau <michellelkys@gmail.com>
2018-07-11 16:33:45 +0800

    extract district

---
+++ extract_district.js
@@ -0,0 +1,16 @@
+
+let elements = document.querySelectorAll('.btn'); // Nodelist
+let districtId = [];
+let districtName = [];
+
+let districtElements = Array.from(elements).filter(e => e.getAttribute('class') == 'btn'); 
+
+for (let i = 0; i < districtElements.length; i++) {
+    districtId.push(districtElements[i].getAttribute('data-param'));
+    districtName.push(districtElements[i].getAttribute('data-tag'));
+}
+
+
+var result = {};
+districtId.forEach((id, i) => result[id] = districtName[i]);
+console.log(result);
